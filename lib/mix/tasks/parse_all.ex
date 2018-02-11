5de9bc1f1442b0f662658407b7cd60d58a096823
Sweetlie <sweetlie@wipeinc.io>
2018-02-11 20:01:03 +0100

    add parse_all task

---
+++ lib/mix/tasks/parse_all.ex
@@ -0,0 +1,18 @@
+defmodule Mix.Tasks.Pins.ParseAll do
+  @moduledoc false
+
+  require Logger
+  use Mix.Task
+  alias SeedRaid.Discord.PinnedPost
+
+  def run(_) do
+    Mix.Task.run("app.start")
+
+    :seed_raid
+    |> Application.get_env(:channels)
+    |> Enum.each(fn {channel_id, _} ->
+      PinnedPost.all(channel_id)
+      :timer.sleep(3000)
+    end)
+  end
+end
