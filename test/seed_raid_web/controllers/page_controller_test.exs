d9890d89302f654de06467b36c1dd5c482b9873d
Sweetlie <sweetlie@wipeinc.io>
2018-02-17 18:43:39 +0100

    fix warning

--- test/seed_raid_web/controllers/page_controller_test.exs
+++ test/seed_raid_web/controllers/page_controller_test.exs
@@ -1,7 +1,7 @@
 defmodule SeedRaidWeb.PageControllerTest do
   use SeedRaidWeb.ConnCase
 
-  test "GET /", %{conn: conn} do
+  test "GET /", %{conn: _conn} do
     # conn = get conn, "/"
     # assert html_response(conn, 200) =~ "Welcome to Phoenix!"
   end
