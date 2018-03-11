31d7418202c97ef3f4d34354cc507f18c8a97ecf
Sweetlie <sweetlie@wipeinc.io>
2018-03-11 13:49:18 +0100

    add tz to time

--- assets/js/channels.js
+++ assets/js/channels.js
@@ -2,36 +2,36 @@
 const CHANNELS = {
   eu: {
     timezone: 'CET',
-    format: 'DD MMMM YYYY HH:mm',
+    format: 'DD MMMM YYYY HH:mm z',
   },
 
   'eu-alliance': {
     timezone: 'CET',
     region: 'na',
     side: 'alliance',
-    format: 'DD MMMM YYYY HH:mm',
+    format: 'DD MMMM YYYY HH:mm z',
   },
   'eu-horde': {
     timezone: 'CET',
     region: 'eu',
     side: 'horde',
-    format: 'DD MMMM YYYY HH:mm',
+    format: 'DD MMMM YYYY HH:mm z',
   },
   na: {
     timezone: 'America/New_York',
-    format: 'MMMM DD, YYYY hh:mm a',
+    format: 'MMMM DD, YYYY hh:mm a z',
   },
   'na-alliance': {
     timezone: 'America/New_York',
     region: 'na',
     side: 'alliance',
-    format: 'MMMM DD, YYYY hh:mm a',
+    format: 'MMMM DD, YYYY hh:mm a z',
   },
   'na-horde': {
     timezone: 'America/New_York',
     region: 'na',
     side: 'horde',
-    format: 'MMMM DD, YYYY hh:mm a',
+    format: 'MMMM DD, YYYY hh:mm a z',
   },
 };
 
